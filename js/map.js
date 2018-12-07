3e94d14ceb31d42fdfee424d88abf1fe685d9ee7
Vladimir Kovalkin <vakovalkin@yandex.ru>
2018-12-07 21:47:39 +0300

    Добавляет листнеры по кейдаунам и генерацию данных для карт

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 'use strict';
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var BIG_PIN = 200;
 var BIG_PIN_HALF = BIG_PIN / 2;
 var PIN_WIDTH = 65;
@@ -90,8 +92,7 @@ for (i = 0; i <= AVATAR_NUM.length - 1; i++) {
     guests: getRandomMinMaxNum(2, 10),
     checkin: getRandomArr(CHECKIN),
     checkout: getRandomArr(CHECKOUT),
-    features: getRandomArrLength(FEATURES),
-    photos: getShuffleArray(PHOTOS),
+    photos: getRandomArr(PHOTOS),
     description: ''
   };
 
@@ -102,7 +103,6 @@ for (i = 0; i <= AVATAR_NUM.length - 1; i++) {
   var roomsGuests = offer.rooms + ' комнат(ы) для ' + offer.guests + ' гостей';
   var randomLocation = 'left: ' + pinLocation.x + 'px' + '; top: ' + pinLocation.y + 'px';
   var pictureAlt = TITLE[i];
-  // var pinId = idCounter();
   var pinObj = {
     location: randomLocation,
     source: author.avatar,
@@ -113,66 +113,84 @@ for (i = 0; i <= AVATAR_NUM.length - 1; i++) {
     appartment: appartmentCheck(randomArr),
     roomGuest: roomsGuests,
     check: checkinCheckout,
-    photosSrc: offer.photos,
+    photosSrc: getShuffleArray(PHOTOS),
+    features: getRandomArrLength(FEATURES),
   };
   mapArr.push(pinObj);
 }
+
 var renderPin = function (pin) {
   var pinElement = mapPinTemplate.cloneNode(true);
-
   pinElement.dataset.pinId = i;
-
   pinElement.style = pin.location;
   pinElement.querySelector('img').src = pin.source;
   pinElement.querySelector('img').alt = pin.altText;
+  pinElement.addEventListener('click', function (evt) {
+    refreshCard(mapArr[evt.currentTarget.dataset.pinId]);
+    pinElement.addEventListener('keydown', function (enterEvt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        enterEvt.refreshCard(mapArr[evt.currentTarget.dataset.pinId]);
+      }
+    });
+  });
   return pinElement;
 };
+
 var fragment = document.createDocumentFragment();
 for (i = 0; i < mapArr.length; i++) {
-  fragment.appendChild(renderPin(mapArr[i]));
+  fragment.appendChild(renderPin(mapArr[i], i));
 }
-
-// mapPin.addEventListener('click', function(evt) {
-//   var dinamId = evt.target.dataset.pinId;
-// })
-
 
 var renderCard = function () {
   var cardElement = cardTemplate.cloneNode(true);
+  cardElement.classList.add('hidden');
+  cardElement.querySelector('.popup__close').addEventListener('click', function () {
+    cardElement.classList.add('hidden');
+  });
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      cardElement.classList.add('hidden');
+    }
+  });
+  return cardElement;
+};
 
-  cardElement.querySelector('.popup__title').textContent = pinObj.titleArr;
-  cardElement.querySelector('.popup__text--address').textContent = pinObj.address;
-  cardElement.querySelector('.popup__text--price').textContent = pinObj.price + ' ' + '₽/ночь';
-  cardElement.querySelector('.popup__type').textContent = pinObj.appartment;
-  cardElement.querySelector('.popup__text--capacity').textContent = pinObj.roomGuest;
-  cardElement.querySelector('.popup__text--time').textContent = pinObj.check;
-  var listElement = cardElement.querySelector('.popup__features');
+var refreshCard = function (pin) {
+  var card = document.querySelector('.map .map__card');
+  card.classList.remove('hidden');
+  card.querySelector('.popup__title').textContent = pin.titleArr;
+  card.querySelector('.popup__text--address').textContent = pin.address;
+  card.querySelector('.popup__text--price').textContent = pin.price + ' ' + '₽/ночь';
+  card.querySelector('.popup__type').textContent = pin.appartment;
+  card.querySelector('.popup__text--capacity').textContent = pin.roomGuest;
+  card.querySelector('.popup__text--time').textContent = pin.check;
+  card.querySelector('.popup__description').textContent = pin.description;
+  card.querySelector('.popup__avatar').src = pin.source;
+  var listElement = card.querySelector('.popup__features');
   listElement.innerHTML = '';
-  for (var j = 0; j < offer.features.length; j++) {
+  for (var j = 0; j < pin.features.length; j++) {
     var newElement = document.createElement('li');
     newElement.classList.add('popup__feature');
-    newElement.classList.add('popup__feature--' + offer.features[j]);
+    newElement.classList.add('popup__feature--' + pin.features[j]);
     listElement.appendChild(newElement);
   }
-  cardElement.querySelector('.popup__description').textContent = mapArr.description;
-  var oldPhotoElement = cardElement.querySelector('.popup__photos');
-
-  for (var k = 0; k < pinObj.photosSrc.length - 1; k++) {
-    var newPhotoElement = cardElement.querySelector('.popup__photo');
-    newPhotoElement.src = pinObj.photosSrc[2];
-    newPhotoElement = newPhotoElement.cloneNode(true);
-    newPhotoElement.src = pinObj.photosSrc[k];
+  var oldPhotoElement = card.querySelector('.popup__photos');
+  oldPhotoElement.innerHTML = '';
+  for (var k = 0; k < pin.photosSrc.length; k++) {
+    var newPhotoElement = card.querySelector('.popup__photo');
+    newPhotoElement = document.createElement('img');
+    newPhotoElement.src = pin.photosSrc[k];
+    newPhotoElement.alt = 'Фотография жилья';
+    newPhotoElement.style.width = '45px';
+    newPhotoElement.style.height = '40px';
+    newPhotoElement.classList.add('popup__photo');
     oldPhotoElement.appendChild(newPhotoElement);
   }
-  cardElement.querySelector('.popup__avatar').src = author.avatar;
-  return cardElement;
-
 };
 
 var fragmentCard = document.createDocumentFragment();
 fragmentCard.appendChild(renderCard());
 
-// var pinCoordinates = [];
 var mainPin = document.querySelector('.map__pin');
 mainPin.addEventListener('mouseup', function (evt) {
   evt.stopPropagation();
